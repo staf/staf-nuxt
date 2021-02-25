52520373111bb986f0412d99eeb1e960e21c4a07
Linus Staf <linus@staf.io>
2021-02-25 13:52:44 +0100

    FAvicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
     ],
   },
   /*
